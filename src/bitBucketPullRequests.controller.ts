590e9bb85456a3a5891a51b0876e8f4a02e4eaf3
Jan Kratochvil <jan.kratochvil@postman.com>
2022-09-20 18:14:10 +0200

    Prettier Slack-friendly formatting

--- src/bitBucketPullRequests.controller.ts
+++ src/bitBucketPullRequests.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { formatDistance, parseISO } from 'date-fns';
+
 import { BitBucketService } from './bitBucket.service';
+import { range } from './utils/number';
 
 type GetResponse = {
   pullRequests: {
@@ -8,6 +10,19 @@ type GetResponse = {
     url: string;
     created: string;
   }[];
+};
+
+const getWithFixedLength = (name: string, length: number) => {
+  if (name.length > length) {
+    return `${name.substring(0, length - 2)}... `;
+  }
+
+  const whitespace = range(0, length - name.length).reduce(
+    (previous) => `${previous} `,
+    '',
+  );
+
+  return name + whitespace + ' ';
 };
 
 @Controller('bitbucket/pulls/opened')
@@ -19,20 +34,52 @@ export class BitBucketPullRequestsController {
   async get(): Promise<GetResponse> {
     const pullRequests = await this.bitBucketService.getPRsForUser();
 
-    const pullRequestsFormatted = pullRequests.map((x) => ({
-      name: x.title,
-      url: x.links.html.href,
-      created: formatDistance(parseISO(x.created_on), new Date()),
-      reviewers:
-        x.reviewers.length > 0
-          ? x.reviewers
-              .map((x) => x.nickname)
-              .reduce((previous, current) => `${previous}, ${current}`)
-          : '⚠️ no reviewers',
-    }));
+    const pullRequestsFormatted = pullRequests
+      .sort(
+        (a, b) =>
+          parseISO(a.created_on).getTime() - parseISO(b.created_on).getTime(),
+      )
+      .map((x) => ({
+        name: x.title,
+        url: x.links.html.href,
+        created: `from ${formatDistance(
+          parseISO(x.created_on),
+          new Date(),
+        )} ago`,
+        reviewers:
+          x.reviewers.length > 0
+            ? x.reviewers
+                .map((x) => x.nickname.split(/[\.\s]/)[0])
+                .map((x) => x[0].toUpperCase() + x.slice(1))
+                .reduce((previous, current) => `${previous}, ${current}`)
+            : '⚠️ no reviewers',
+      }));
+
+    const longestTitleLength = Math.max(
+      ...pullRequestsFormatted.map((x) => x.name.length),
+    );
+
+    const longestCreatedLength = Math.max(
+      ...pullRequestsFormatted.map((x) => x.created.length),
+    );
+
+    const longestReviewersLength = Math.max(
+      ...pullRequestsFormatted.map((x) => x.reviewers.length),
+    );
+
+    const effectiveLongestTitleLength = Math.min(longestTitleLength, 50);
+
+    const pullRequestsFormattedTabular = pullRequestsFormatted.map(
+      ({ name, url, created, reviewers }) => ({
+        name: getWithFixedLength(name, effectiveLongestTitleLength),
+        url,
+        created: getWithFixedLength(created, longestCreatedLength),
+        reviewers: getWithFixedLength(reviewers, longestReviewersLength),
+      }),
+    );
 
     return {
-      pullRequests: pullRequestsFormatted,
+      pullRequests: pullRequestsFormattedTabular,
     };
   }
 }
